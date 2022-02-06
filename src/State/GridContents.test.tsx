e858b3832368906ec5abdab22c60f792855dafc1
Daniel Howlett <dan@dancras.co.uk>
2022-02-06 15:05:32 +0000

    Store valid answer state in GridContents and use SudokuRules to set it

--- src/State/GridContents.test.tsx
+++ src/State/GridContents.test.tsx
@@ -1,36 +1,47 @@
 import { act, render, screen } from '@testing-library/react';
+import { MockProxy, mock } from 'jest-mock-extended';
 import { useContext, memo } from 'react';
 import { emptyGridContents, updateGridContents, GridContentsProvider, GridContentsContext, GridContentsReducer, GridContentsCell } from 'src/State/GridContents';
+import SudokuRules from 'src/Sudoku/SudokuRules';
 
 describe('updateGridContents', () => {
+    let testRules: MockProxy<SudokuRules>;
+
+    beforeEach(() => {
+        testRules = mock<SudokuRules>();
+
+        // Make every answer valid by default
+        testRules.setContents.mockReturnValue([]);
+    });
+
     test('action toggleContents sets cell contents if cell is empty or different', () => {
         const initialState = emptyGridContents();
-        const secondState = updateGridContents(initialState, {
+        const secondState = updateGridContents(testRules, initialState, {
             action: 'toggleContents',
             cell: 0,
             contents: 5
         });
 
-        expect(secondState[0].contents).toEqual(5);
+        expect(secondState[0].contents?.[0]).toEqual(5);
 
-        const thirdState = updateGridContents(secondState, {
+        const thirdState = updateGridContents(testRules, secondState, {
             action: 'toggleContents',
             cell: 0,
             contents: 6
         });
 
-        expect(thirdState[0].contents).toEqual(6);
+        expect(thirdState[0].contents?.[0]).toEqual(6);
     });
 
     test('action toggleContents clears cell contents if current value matches', () => {
         const initialState = emptyGridContents();
-        const secondState = updateGridContents(initialState, {
+        const secondState = updateGridContents(testRules, initialState, {
             action: 'toggleContents',
             cell: 0,
             contents: 5
         });
 
-        const thirdState = updateGridContents(secondState, {
+        const thirdState = updateGridContents(testRules, secondState, {
             action: 'toggleContents',
             cell: 0,
             contents: 5
@@ -41,7 +52,9 @@ describe('updateGridContents', () => {
 
     test('action toggleCandidate toggles the candidate flag for the given cell and candidate number', () => {
         const initialState = emptyGridContents();
-        const secondState = updateGridContents(initialState, {
+        testRules.isValidCandidate.mockReturnValue(true);
+
+        const secondState = updateGridContents(testRules, initialState, {
             action: 'toggleCandidate',
             cell: 0,
             candidate: 5
@@ -49,13 +62,95 @@ describe('updateGridContents', () => {
 
         expect(secondState[0].candidates[5]).toEqual(true);
 
-        const thirdState = updateGridContents(secondState, {
+        const thirdState = updateGridContents(testRules, secondState, {
             action: 'toggleCandidate',
             cell: 0,
             candidate: 5
         });
 
-        expect(thirdState[0].candidates[5]).toEqual(false);
+        expect(thirdState[0].candidates[5]).toEqual(null);
+    });
+
+    test('action toggleContents marks contents as invalid according to rules', () => {
+        const initialState = emptyGridContents();
+        // Valid -> Invalid
+        initialState[0].contents = [2, true];
+        // Valid -> Valid
+        initialState[1].contents = [3, true];
+        // Invalid -> Valid
+        initialState[2].contents = [4, false];
+        // Invalid -> Invalid
+        initialState[3].contents = [5, false];
+
+        testRules.setContents.mockReturnValue([0, 1, 2, 3]);
+        testRules.isValidContents.calledWith(0).mockReturnValue(false);
+        testRules.isValidContents.calledWith(1).mockReturnValue(true);
+        testRules.isValidContents.calledWith(2).mockReturnValue(true);
+        testRules.isValidContents.calledWith(3).mockReturnValue(false);
+
+        const secondState = updateGridContents(testRules, initialState, {
+            action: 'toggleContents',
+            cell: 20,
+            contents: 5
+        });
+
+        expect(testRules.setContents).toHaveBeenCalledWith(20, 5);
+
+        expect(secondState[0].contents).toEqual([2, false]);
+        expect(secondState[1].contents).toEqual([3, true]);
+        expect(secondState[2].contents).toEqual([4, true]);
+        expect(secondState[3].contents).toEqual([5, false]);
+    });
+
+    test('action toggleContents marks candidates as invalid according to rules', () => {
+        const initialState = emptyGridContents();
+        // Valid -> Invalid
+        initialState[0].candidates[1] = true;
+        // Valid -> Valid
+        initialState[0].candidates[2] = true;
+        // Invalid -> Invalid
+        initialState[0].candidates[3] = false;
+        // Invalid -> Value
+        initialState[0].candidates[4] = false;
+
+        testRules.setContents.mockReturnValue([0]);
+        testRules.isValidCandidate.calledWith(0, 1).mockReturnValue(false);
+        testRules.isValidCandidate.calledWith(0, 2).mockReturnValue(true);
+        testRules.isValidCandidate.calledWith(0, 3).mockReturnValue(false);
+        testRules.isValidCandidate.calledWith(0, 4).mockReturnValue(true);
+
+        const secondState = updateGridContents(testRules, initialState, {
+            action: 'toggleContents',
+            cell: 20,
+            contents: 5
+        });
+
+        expect(secondState[0].candidates[1]).toEqual(false);
+        expect(secondState[0].candidates[2]).toEqual(true);
+        expect(secondState[0].candidates[3]).toEqual(false);
+        expect(secondState[0].candidates[4]).toEqual(true);
+    });
+
+    test('action toggleCandidate marks the new candidate as valid according to rules', () => {
+        const initialState = emptyGridContents();
+
+        testRules.isValidCandidate.calledWith(20, 5).mockReturnValue(true);
+        testRules.isValidCandidate.calledWith(21, 6).mockReturnValue(false);
+
+        const secondState = updateGridContents(testRules, initialState, {
+            action: 'toggleCandidate',
+            cell: 20,
+            candidate: 5
+        });
+
+        const thirdState = updateGridContents(testRules, secondState, {
+            action: 'toggleCandidate',
+            cell: 21,
+            candidate: 6
+        });
+
+        expect(thirdState[20].candidates[5]).toEqual(true);
+        expect(thirdState[21].candidates[6]).toEqual(false);
     });
 });
 
@@ -65,7 +160,7 @@ test('the grid contents and a dispatch fn is shared via context', function () {
     function ExampleConsumer() {
         const [gridContents, dispatch] = useContext(GridContentsContext);
         dispatchGridContentsUpdate = dispatch;
-        return <div>{gridContents[0].contents}</div>;
+        return <div>{gridContents[0].contents?.[0]}</div>;
     }
 
     render(
@@ -91,8 +186,8 @@ test('consumers of individual cell data do not re-render for unrelated changes',
     const renderSpy = jest.fn();
 
     function CellConsumer({ cell }: { cell: GridContentsCell }) {
-        renderSpy(cell.contents);
-        return <div>{cell.contents}</div>;
+        renderSpy(cell.contents?.[0]);
+        return <div>{cell.contents?.[0]}</div>;
     }
 
     const MemoizedCellConsumer = memo(CellConsumer);
--- src/State/GridContents.tsx
+++ src/State/GridContents.tsx
@@ -1,14 +1,17 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useMemo, useReducer } from 'react';
 import produce from 'immer';
 import { ReactReducer } from 'src/State/UtilityTypes';
+import SudokuRules from 'src/Sudoku/SudokuRules';
 
 export type GridContentsReducer = ReactReducer<GridContents, GridContentsUpdate>;
 
 export const GridContentsContext = createContext<GridContentsReducer>([emptyGridContents(), () => void(0)]);
 
+export type Answer = [number, boolean];
+
 export type GridContentsCell = {
-    contents: number | null,
-    candidates: Record<number, boolean>
+    contents: Answer | null,
+    candidates: Record<number, boolean | null>
 }
 
 export type GridContents = GridContentsCell[];
@@ -27,15 +30,15 @@ function emptyGridContentsCell(): GridContentsCell {
     return {
         contents: null,
         candidates: {
-            1: false,
-            2: false,
-            3: false,
-            4: false,
-            5: false,
-            6: false,
-            7: false,
-            8: false,
-            9: false
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+            7: null,
+            8: null,
+            9: null
         }
     };
 }
@@ -44,20 +47,41 @@ export function emptyGridContents(): GridContents {
     return Array.from({ length: 81 }).map(emptyGridContentsCell);
 }
 
-export function updateGridContents(currentState: GridContents, update: GridContentsUpdate): GridContents {
+export function updateGridContents(rules: SudokuRules, currentState: GridContents, update: GridContentsUpdate): GridContents {
     return produce(currentState, draft => {
         const cell = draft[update.cell];
 
         if (update.action === 'toggleContents') {
-            cell.contents = cell.contents === update.contents ? null : update.contents;
+            cell.contents = cell.contents?.[0] === update.contents ? null : [update.contents, true];
+
+            const affectedCells = rules.setContents(update.cell, cell.contents ? cell.contents[0] : null);
+
+            affectedCells.forEach(i => {
+                const contents = draft[i].contents;
+                if (contents !== null) {
+                    contents[1] = rules.isValidContents(i);
+                }
+
+                for (const key in draft[i].candidates) {
+                    const j = parseInt(key);
+                    if (draft[i].candidates[j] !== null) {
+                        draft[i].candidates[j] = rules.isValidCandidate(i, j);
+                    }
+                }
+            });
+
         } else if (update.action === 'toggleCandidate') {
-            cell.candidates[update.candidate] = !cell.candidates[update.candidate];
+
+            cell.candidates[update.candidate] = cell.candidates[update.candidate] === null ?
+                rules.isValidCandidate(update.cell, update.candidate) :
+                null;
         }
     });
 }
 
 export function GridContentsProvider({ children }: React.PropsWithChildren<unknown>) {
-    const gridContentsReducer = useReducer(updateGridContents, emptyGridContents());
+    const rules = useMemo(() => new SudokuRules(), []);
+    const gridContentsReducer = useReducer((state: GridContents, update: GridContentsUpdate) => updateGridContents(rules, state, update), emptyGridContents());
     return (
         <GridContentsContext.Provider value={gridContentsReducer}>
             {children}
